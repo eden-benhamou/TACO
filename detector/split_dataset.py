ef4ba6595926844d503e70c483747099503e5cef
Eden Benhamou <edencoza@gmail.com>
2023-03-13 21:32:40 +0200

    Change split_dataset.py to split only train data to train and val

--- detector/split_dataset.py
+++ detector/split_dataset.py
@@ -8,55 +8,46 @@ import copy
 
 parser = argparse.ArgumentParser(description='User args')
 parser.add_argument('--dataset_dir', required=True, help='Path to dataset annotations')
-parser.add_argument('--test_percentage', type=int, default=10, required=False, help='Percentage of images used for the testing set')
+#parser.add_argument('--test_percentage', type=int, default=10, required=False, help='Percentage of images used for the testing set')
 parser.add_argument('--val_percentage', type=int, default=10, required=False, help='Percentage of images used for the validation set')
-parser.add_argument('--nr_trials', type=int, default=10, required=False, help='Number of splits')
+parser.add_argument('--nr_trials', type=int, default=1, required=False, help='Number of splits')
 
 args = parser.parse_args()
 
-ann_input_path = args.dataset_dir + '/' + 'annotations.json'
+train_ann_input_path = args.dataset_dir + '/' + 'annotations_train.json'
 
-# Load annotations
-with open(ann_input_path, 'r') as f:
-    dataset = json.loads(f.read())
+# Load train annotations
+with open(train_ann_input_path, 'r') as f:
+    train_dataset = json.loads(f.read())
 
-anns = dataset['annotations']
-scene_anns = dataset['scene_annotations']
-imgs = dataset['images']
-nr_images = len(imgs)
+train_anns = train_dataset['annotations']
+#scene_anns = dataset['scene_annotations']
+train_imgs = train_dataset['images']
+nr_train_images = len(train_imgs)
 
-nr_testing_images = int(nr_images*args.test_percentage*0.01+0.5)
-nr_nontraining_images = int(nr_images*(args.test_percentage+args.val_percentage)*0.01+0.5)
+#nr_testing_images = int(nr_images*args.test_percentage*0.01+0.5)
+nr_nontraining_images = int(nr_train_images*(args.val_percentage)*0.01+0.5)
 
 
 for i in range(args.nr_trials):
-    random.shuffle(imgs)
+    random.shuffle(train_imgs)
 
     # Add new datasets
-    train_set = {
-        'info': None,
-        'images': [],
-        'annotations': [],
-        'scene_annotations': [],
-        'licenses': [],
-        'categories': [],
-        'scene_categories': [],
-    }
-    train_set['info'] =  dataset['info']
-    train_set['categories'] = dataset['categories']
-    train_set['scene_categories'] = dataset['scene_categories']
+    train_set = {'info': train_dataset['info'], 'images': [], 'annotations': [],
+                 'licenses': [], 'categories': train_dataset['categories']}
 
     val_set = copy.deepcopy(train_set)
     test_set = copy.deepcopy(train_set)
 
-    test_set['images'] = imgs[0:nr_testing_images]
-    val_set['images'] = imgs[nr_testing_images:nr_nontraining_images]
-    train_set['images'] = imgs[nr_nontraining_images:nr_images]
+    # test_set['images'] = test_dataset['images']
+    val_set['images'] = train_imgs[0:nr_nontraining_images]
+    train_set['images'] = train_imgs[nr_nontraining_images:nr_train_images]
 
     # Aux Image Ids to split annotations
-    test_img_ids, val_img_ids, train_img_ids = [],[],[]
-    for img in test_set['images']:
-        test_img_ids.append(img['id'])
+    #test_img_ids, val_img_ids, train_img_ids = [],[],[]
+    val_img_ids, train_img_ids = [], []
+    # for img in test_set['images']:
+    #     test_img_ids.append(img['id'])
 
     for img in val_set['images']:
         val_img_ids.append(img['id'])
@@ -65,27 +56,18 @@ for i in range(args.nr_trials):
         train_img_ids.append(img['id'])
 
     # Split instance annotations
-    for ann in anns:
-        if ann['image_id'] in test_img_ids:
-            test_set['annotations'].append(ann)
-        elif ann['image_id'] in val_img_ids:
+    for ann in train_anns:
+        # if ann['image_id'] in test_img_ids:
+        #     test_set['annotations'].append(ann)
+        if ann['image_id'] in val_img_ids:
             val_set['annotations'].append(ann)
         elif ann['image_id'] in train_img_ids:
             train_set['annotations'].append(ann)
 
-    # Split scene tags
-    for ann in scene_anns:
-        if ann['image_id'] in test_img_ids:
-            test_set['scene_annotations'].append(ann)
-        elif ann['image_id'] in val_img_ids:
-            val_set['scene_annotations'].append(ann)
-        elif ann['image_id'] in train_img_ids:
-            train_set['scene_annotations'].append(ann)
-
     # Write dataset splits
     ann_train_out_path = args.dataset_dir + '/' + 'annotations_' + str(i) +'_train.json'
     ann_val_out_path   = args.dataset_dir + '/' + 'annotations_' + str(i) + '_val.json'
-    ann_test_out_path  = args.dataset_dir + '/' + 'annotations_' + str(i) + '_test.json'
+    #ann_test_out_path  = args.dataset_dir + '/' + 'annotations_' + str(i) + '_test.json'
 
     with open(ann_train_out_path, 'w+') as f:
         f.write(json.dumps(train_set))
@@ -93,7 +75,7 @@ for i in range(args.nr_trials):
     with open(ann_val_out_path, 'w+') as f:
         f.write(json.dumps(val_set))
 
-    with open(ann_test_out_path, 'w+') as f:
-        f.write(json.dumps(test_set))
+    # with open(ann_test_out_path, 'w+') as f:
+    #     f.write(json.dumps(test_set))
 
 
